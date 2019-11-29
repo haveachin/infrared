ec4b48c4932584d30b592e55124e02df2cdf3a6a
Hendrik J. Schlehlein <hendrik.schlehlein@gmail.com>
2019-11-29 08:16:25 +0100

    Updated infrared

--- cmd/infrared/main.go
+++ cmd/infrared/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/haveachin/infrared"
+)
+
+func main() {
+	proxy := infrared.Proxy{
+		Addr:    ":25565",
+		Configs: map[string]infrared.Config{},
+	}
+
+	configs, err := infrared.ReadConfigs("./configs/")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	for _, config := range configs {
+		proxy.Configs[config.DomainName] = config
+	}
+
+	log.Fatal(proxy.ListenAndServe())
 }
