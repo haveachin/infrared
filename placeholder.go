6fda2e5d2ddcb474834fdceda4b0b4be07e05c86
Hendrik J. Schlehlein <hendrik.schlehlein@gmail.com>
2019-11-29 08:14:54 +0100

    Refactored placeholder

--- placeholder.go
+++ placeholder.go
@@ -1,20 +1,28 @@
 package infrared
 
-type Placeholder struct {
-	Version struct {
-		Name     string `json:"name"`
-		Protocol int    `json:"protocol"`
-	} `json:"version"`
-	Players struct {
-		Max    int `json:"max"`
-		Online int `json:"online"`
-		Sample []struct {
-			Name string `json:"name"`
-			ID   string `json:"id"`
-		} `json:"sample"`
-	} `json:"players"`
-	Description struct {
-		Text string `json:"text"`
-	} `json:"description"`
-	Favicon string `json:"favicon"`
+type placeholder struct {
+	Version     version     `json:"version"`
+	Players     players     `json:"players"`
+	Description description `json:"description"`
+	Favicon     string      `json:"favicon"`
+}
+
+type version struct {
+	Name     string `json:"name"`
+	Protocol int    `json:"protocol"`
+}
+
+type players struct {
+	Max    int      `json:"max"`
+	Online int      `json:"online"`
+	Sample []player `json:"sample"`
+}
+
+type player struct {
+	Name string `json:"name"`
+	ID   string `json:"id"`
+}
+
+type description struct {
+	Text string `json:"text"`
 }
