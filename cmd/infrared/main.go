cf0efa2df7cddec1fbbe79bca9af4b6d7e78bd4c
Hendrik J. Schlehlein <hendrik.schlehlein@gmail.com>
2019-11-29 09:16:39 +0100

    Added EnvVar and Falg support

--- cmd/infrared/main.go
+++ cmd/infrared/main.go
@@ -3,16 +3,48 @@ package main
 import (
 	"log"
 
+	"github.com/spf13/pflag"
+
+	"github.com/spf13/viper"
+
 	"github.com/haveachin/infrared"
 )
 
+const (
+	envPrefix     = "infrared"
+	envAddress    = "address"
+	envConfigPath = "config_path"
+)
+
+func init() {
+	viper.SetEnvPrefix(envPrefix)
+	viper.BindEnv(envAddress)
+	viper.BindEnv(envConfigPath)
+
+	pflag.String(envAddress, ":25565", "address that the proxy listens to")
+	pflag.String(envConfigPath, "./configs/", "path of all your server configs")
+
+	pflag.Parse()
+	viper.BindPFlags(pflag.CommandLine)
+}
+
 func main() {
+	address := viper.Get(envAddress).(string)
+	if address == "" {
+		address = viper.GetString(envAddress)
+	}
+
+	configPath := viper.Get(envConfigPath).(string)
+	if configPath == "" {
+		configPath = viper.GetString(envConfigPath)
+	}
+
 	proxy := infrared.Proxy{
-		Addr:    ":25565",
+		Addr:    address,
 		Configs: map[string]infrared.Config{},
 	}
 
-	configs, err := infrared.ReadConfigs("./configs/")
+	configs, err := infrared.ReadConfigs(configPath)
 	if err != nil {
 		log.Fatal(err)
 		return
