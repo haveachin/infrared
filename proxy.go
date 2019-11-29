ceb373a3dd71874a1bd3db41bf6cb408efca40f9
Hendrik J. Schlehlein <hendrik.schlehlein@gmail.com>
2019-11-29 08:15:53 +0100

    Updated and Refactored proxy

--- proxy.go
+++ proxy.go
@@ -1,7 +1,9 @@
 package infrared
 
 import (
+	"fmt"
 	"log"
+	"strings"
 
 	"github.com/haveachin/infrared/net"
 	"github.com/haveachin/infrared/net/packet"
@@ -13,16 +15,14 @@ type Proxy struct {
 	// TCP address to listen on
 	Addr string
 
-	// Map of domains that routes to server addresses
-	Routes map[string]string
-
-	// Map of domains that routes to place holder packets
-	PlaceHolder map[string][]byte
+	// Map of domains that to configs
+	Configs map[string]Config
 }
 
 // ListenAndServe listens on the TCP network address addr and then handle packets
 // on incoming connections.
 func (s *Proxy) ListenAndServe() error {
+	log.Println("Starting to Listen on", s.Addr)
 	listener, err := net.ListenMC(s.Addr)
 	if err != nil {
 		return err
@@ -43,33 +43,87 @@ func (s *Proxy) serve(listener *net.Listener) error {
 	}
 }
 
-func (s *Proxy) handleConn(conn *net.Conn) {
-	pk, _ := conn.ReadPacket()
-	handshake, _ := packet.ParseHandshake(pk)
-	rconn, err := net.DialMC(s.Routes[string(handshake.ServerAddress)])
+func makeHandshake(client *net.Conn, config Config) error {
+	pk, err := client.ReadPacket()
 	if err != nil {
-		err := conn.WritePacket(packet.Marshal(0x00, packet.String(`{
-    "version": {
-        "name": "1.14.4",
-        "protocol": 498
-    },
-    "players": {
-        "max": 0,
-        "online": 1,
-        "sample": [
-            {
-                "name": "thinkofdeath",
-                "id": "4566e69f-c907-48ee-8d71-d7ba5aa00d20"
-            }
-        ]
-    },	
-    "description": {
-        "text": "Hello world"
-    },
-    "favicon": "data:image/png;base64,<data>"
-}`)))
-		if err != nil {
-			log.Println(err)
+		return err
+	}
+
+	if pk.ID != packet.SLPRequestPacketID {
+		return fmt.Errorf("expexted request packet (%d); got this: %d", packet.SLPRequestPacketID, pk.ID)
+	}
+
+	response := packet.SLPResponse{
+		JSONResponse: packet.String(config.PlaceholderJSON),
+	}
+
+	if err := client.WritePacket(response.Marshal()); err != nil {
+		return err
+	}
+
+	pk, err = client.ReadPacket()
+	if err != nil {
+		return err
+	}
+
+	if pk.ID != packet.SLPPingPacketID {
+		return fmt.Errorf("expexted ping packet (%d); got this: %d", packet.SLPPingPacketID, pk.ID)
+	}
+
+	return client.WritePacket(pk)
+}
+
+func sendDisconnectMessage(client *net.Conn, message, address string) error {
+	pk, err := client.ReadPacket()
+	if err != nil {
+		return err
+	}
+
+	start, err := packet.ParseLoginStart(pk)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("%s[%s] tried to connect over %s", start.Name, client.Socket.RemoteAddr().String(), address)
+
+	message = strings.Replace(message, "$username", string(start.Name), -1)
+
+	disconnect := packet.LoginDisconnect{
+		Reason: packet.Chat(fmt.Sprintf("{\"text\":\"%s\"}", message)),
+	}
+
+	return client.WritePacket(disconnect.Marshal())
+}
+
+func (s *Proxy) handleConn(conn *net.Conn) {
+	pk, err := conn.ReadPacket()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	handshake, err := packet.ParseSLPHandshake(pk)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	config := s.Configs[string(handshake.ServerAddress)]
+
+	rconn, err := net.DialMC(config.ProxyTo)
+	if err != nil {
+		if handshake.RequestsStatus() {
+			if err := makeHandshake(conn, config); err != nil {
+				log.Println("Handshake failed:", err)
+			}
+
+			return
+		}
+
+		address := fmt.Sprintf("%s:%d", handshake.ServerAddress, handshake.ServerPort)
+
+		if err := sendDisconnectMessage(conn, config.DisconnectMessage, address); err != nil {
+			log.Println("Disconnect failed:", err)
 		}
 
 		return
