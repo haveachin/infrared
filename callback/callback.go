01d71d34483b0c0f5ca0df9436cacac15020cff9
Hendrik J. Schlehlein <hendrik.schlehlein@gmail.com>
2020-05-31 08:53:24 +0200

    Added ConsoleWriter and implemented callback logic

--- callback/callback.go
+++ callback/callback.go
@@ -1,5 +1,58 @@
 package callback
 
-func Post(url string, payload Payload) error {
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"net/http"
+)
+
+type ConsoleWriter struct {
+	URL    string
+	Events []Event
+	zerolog.ConsoleWriter
+}
+
+func (w ConsoleWriter) Write(b []byte) (int, error) {
+	go w.handleLog(b)
+	return w.ConsoleWriter.Write(b)
+}
+
+func (w ConsoleWriter) handleLog(b []byte) {
+	eventJSON := struct {
+		Event string `json:"event"`
+	}{}
+
+	if err := json.Unmarshal(b, &eventJSON); err != nil {
+		log.Err(err)
+		return
+	}
+
+	hasEvent := false
+	for _, event := range w.Events {
+		if eventJSON.Event != string(event) {
+			continue
+		}
+
+		hasEvent = true
+		break
+	}
+
+	if !hasEvent {
+		return
+	}
+
+	if err := postToURL(w.URL, b); err != nil {
+		log.Err(err).Str("url", w.URL)
+	}
+}
+
+func postToURL(url string, payload []byte) error {
+	_, err := http.Post(url, "application/json", bytes.NewReader(payload))
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
