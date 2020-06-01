b1e07f10f7a554b289ab7faa8140721778c882ea
Hendrik J. Schlehlein <hendrik.schlehlein@gmail.com>
2020-06-01 08:14:34 +0200

    Renamed process events to container events

--- callback/event.go
+++ callback/event.go
@@ -7,21 +7,21 @@ type Event string
 const EventKey = "event"
 
 const (
-	ErrorEvent          Event = "Error"
-	PlayerJoinEvent     Event = "PlayerJoin"
-	PlayerLeaveEvent    Event = "PlayerLeave"
-	ProcessStartEvent   Event = "ProcessStart"
-	ProcessStopEvent    Event = "ProcessStop"
-	ProcessTimeoutEvent Event = "ProcessTimeout"
+	ErrorEvent            Event = "Error"
+	PlayerJoinEvent       Event = "PlayerJoin"
+	PlayerLeaveEvent      Event = "PlayerLeave"
+	ContainerStartEvent   Event = "ContainerStart"
+	ContainerStopEvent    Event = "ContainerStop"
+	ContainerTimeoutEvent Event = "ContainerTimeout"
 )
 
 var Events = []Event{
 	ErrorEvent,
 	PlayerJoinEvent,
 	PlayerLeaveEvent,
-	ProcessStartEvent,
-	ProcessStopEvent,
-	ProcessTimeoutEvent,
+	ContainerStartEvent,
+	ContainerStopEvent,
+	ContainerTimeoutEvent,
 }
 
 func ParseEvent(name string) (Event, error) {
