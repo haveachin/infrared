61a2febc0a9e6882d0a5a5c108bb2ef49fafc669
Hendrik Jonas Schlehlein <hendrik.schlehlein@gmail.com>
2021-04-08 23:27:04 +0200

    test: add VarInt Encode and Decode

--- protocol/types_test.go
+++ protocol/types_test.go
@@ -71,3 +71,76 @@ func TestBoolean_Decode(t *testing.T) {
 		}
 	}
 }
+
+func TestVarInt_Encode(t *testing.T) {
+	tt := []struct {
+		given    VarInt
+		expected []byte
+	}{
+		{
+			given:    VarInt(0),
+			expected: []byte{0x00},
+		},
+		{
+			given:    VarInt(127),
+			expected: []byte{0x7f},
+		},
+		{
+			given:    VarInt(128),
+			expected: []byte{0x80, 0x01},
+		},
+		{
+			given:    VarInt(2097151),
+			expected: []byte{0xff, 0xff, 0x7f},
+		},
+		{
+			given:    VarInt(-1),
+			expected: []byte{0xff, 0xff, 0xff, 0xff, 0x0f},
+		},
+	}
+
+	for _, tc := range tt {
+		if !bytes.Equal(tc.given.Encode(), tc.expected) {
+			t.Fail()
+		}
+	}
+}
+
+func TestVarInt_Decode(t *testing.T) {
+	tt := []struct {
+		given    []byte
+		expected VarInt
+	}{
+		{
+			given:    []byte{0x00},
+			expected: VarInt(0),
+		},
+		{
+			given:    []byte{0x7f},
+			expected: VarInt(127),
+		},
+		{
+			given:    []byte{0x80, 0x01},
+			expected: VarInt(128),
+		},
+		{
+			given:    []byte{0xff, 0xff, 0x7f},
+			expected: VarInt(2097151),
+		},
+		{
+			given:    []byte{0xff, 0xff, 0xff, 0xff, 0x0f},
+			expected: VarInt(-1),
+		},
+	}
+
+	for _, tc := range tt {
+		var actual VarInt
+		if err := actual.Decode(bytes.NewReader(tc.given)); err != nil {
+			t.Error(err)
+		}
+
+		if actual != tc.expected {
+			t.Fail()
+		}
+	}
+}
