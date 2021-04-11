d0ed59181a7543b66fd6a29c58098fd862f0ac21
Hendrik Jonas Schlehlein <hendrik.schlehlein@gmail.com>
2021-04-11 22:04:12 +0200

    refactor: remove unused code

--- conn.go
+++ conn.go
@@ -24,10 +24,9 @@ type PacketPeeker interface {
 type conn struct {
 	net.Conn
 
-	r         *bufio.Reader
-	w         io.Writer
-	state     protocol.State
-	threshold int
+	r     *bufio.Reader
+	w     io.Writer
+	state protocol.State
 }
 
 type Listener struct {
@@ -53,19 +52,14 @@ type Conn interface {
 	PacketWriter
 	PacketReader
 	PacketPeeker
-
-	State() protocol.State
-	Threshold() int
 }
 
 // wrapConn warp an net.Conn to plasma.Conn
 func wrapConn(c net.Conn) *conn {
 	return &conn{
-		Conn:      c,
-		r:         bufio.NewReader(c),
-		w:         c,
-		state:     protocol.StateHandshaking,
-		threshold: 0,
+		Conn: c,
+		r:    bufio.NewReader(c),
+		w:    c,
 	}
 }
 
@@ -93,11 +87,6 @@ func (c conn) State() protocol.State {
 	return c.state
 }
 
-// Threshold returns the number of bytes a Packet can be long before it will be compressed
-func (c conn) Threshold() int {
-	return c.threshold
-}
-
 func (c *conn) Read(b []byte) (int, error) {
 	return c.r.Read(b)
 }
@@ -108,17 +97,17 @@ func (c *conn) Write(b []byte) (int, error) {
 
 // ReadPacket read a Packet from Conn.
 func (c *conn) ReadPacket() (protocol.Packet, error) {
-	return protocol.ReadPacket(c.r, c.threshold > 0)
+	return protocol.ReadPacket(c.r)
 }
 
 // PeekPacket peeks a Packet from Conn.
 func (c *conn) PeekPacket() (protocol.Packet, error) {
-	return protocol.PeekPacket(c.r, c.threshold > 0)
+	return protocol.PeekPacket(c.r)
 }
 
 //WritePacket write a Packet to Conn.
 func (c *conn) WritePacket(p protocol.Packet) error {
-	pk, err := p.Marshal(c.threshold)
+	pk, err := p.Marshal()
 	if err != nil {
 		return err
 	}
