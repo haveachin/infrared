cbca200314d194709290a82ad5bc8c9904156439
Hendrik Jonas Schlehlein <hendrik.schlehlein@gmail.com>
2021-04-12 02:06:23 +0200

    test: add Scan and Marshal Fields tests

--- protocol/packet_test.go
+++ protocol/packet_test.go
@@ -68,3 +68,54 @@ func TestPacket_Scan(t *testing.T) {
 		t.Errorf("got: %x; want: %x", byteField.Encode(), 0xf2)
 	}
 }
+
+func TestScanFields(t *testing.T) {
+	// Arrange
+	packet := Packet{
+		ID:   0x00,
+		Data: []byte{0x00, 0xf2},
+	}
+
+	var booleanField Boolean
+	var byteField Byte
+
+	// Act
+	err := ScanFields(
+		bytes.NewReader(packet.Data),
+		&booleanField,
+		&byteField,
+	)
+
+	// Assert
+	if err != nil {
+		t.Error(err)
+	}
+
+	if booleanField != false {
+		t.Error("got: true; want: false")
+	}
+
+	if !bytes.Equal(byteField.Encode(), []byte{0xf2}) {
+		t.Errorf("got: %x; want: %x", byteField.Encode(), 0xf2)
+	}
+}
+
+func TestMarshalPacket(t *testing.T) {
+	// Arrange
+	packetId := byte(0x00)
+	booleanField := Boolean(false)
+	byteField := Byte(0x0f)
+	packetData := []byte{0x00, 0x0f}
+
+	// Act
+	packet := MarshalPacket(packetId, booleanField, byteField)
+
+	// Assert
+	if packet.ID != packetId {
+		t.Errorf("packet id: got: %v; want: %v", packet.ID, packetId)
+	}
+
+	if !bytes.Equal(packet.Data, packetData) {
+		t.Errorf("got: %v; want: %v", packet.Data, packetData)
+	}
+}
