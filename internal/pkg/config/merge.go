7b487c2cbbae0aa4ea9e0a290709bc7c2ce11b15
Dominik Ochs <dominik.ochs@gmx.net>
2022-10-30 17:55:28 +0100

    fix: implement custom config merger  (#159)
    * fix: add custom config merger
    
    * chore: refactor test and implement new merge func
    
    Co-authored-by: Hendrik Schlehlein <hendrik.schlehlein@gmail.com>

---
+++ internal/pkg/config/merge.go
@@ -0,0 +1,60 @@
+package config
+
+import (
+	"fmt"
+)
+
+// mergeSlices only merges slices of primitives.
+func mergeSlices(base, other []any) []any {
+	lookup := map[any]bool{}
+
+	for _, v := range base {
+		lookup[v] = true
+	}
+
+	for _, v := range other {
+		if _, ok := lookup[v]; !ok {
+			continue
+		}
+
+		base = append(base, v)
+	}
+
+	return base
+}
+
+func MergeConfigsMaps(base, other map[string]any) (map[string]any, error) {
+	merged := map[string]any{}
+
+	for k, v := range base {
+		// no collision, just copy
+		if _, ok := other[k]; !ok {
+			merged[k] = v
+			continue
+		}
+
+		// collision
+		switch casted := v.(type) {
+		case map[string]any:
+			m, err := MergeConfigsMaps(casted, other[k].(map[string]any))
+			if err != nil {
+				return merged, err
+			}
+			merged[k] = m
+		case []any:
+			merged[k] = mergeSlices(casted, other[k].([]any))
+		case string, float64, int:
+			merged[k] = v
+		default:
+			return merged, fmt.Errorf("error during config merge: type %T not implemented", v)
+		}
+	}
+
+	for k, v := range other {
+		if _, ok := base[k]; !ok {
+			merged[k] = v
+		}
+	}
+
+	return merged, nil
+}
