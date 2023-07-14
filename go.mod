bed823f0ff1d49f8dc4b3edbb48017aefbf9ba4f
Marcel D. Juhnke <marrat@marrat.de>
2023-07-14 10:56:04 +0200

    feat: add kubernetes config provider (#182)
    * add Kubernetes config provider
    
    * remove unneeded output
    
    * replace errors.Wrap() with fmt.Errorf()
    
    * add deferred K8s watcher.Stop()
    
    * remove errors package from go.mod

--- go.mod
+++ go.mod
@@ -31,23 +31,40 @@ require (
 	go.uber.org/zap v1.24.0
 	golang.org/x/net v0.10.0
 	gopkg.in/yaml.v3 v3.0.1
+	k8s.io/apimachinery v0.27.3
+	k8s.io/client-go v0.27.3
 )
 
 require (
 	github.com/Microsoft/go-winio v0.6.1 // indirect
 	github.com/ajg/form v1.5.1 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/docker/distribution v2.8.2+incompatible // indirect
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-units v0.5.0 // indirect
+	github.com/emicklei/go-restful/v3 v3.9.0 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-openapi/jsonpointer v0.19.6 // indirect
+	github.com/go-openapi/jsonreference v0.20.1 // indirect
+	github.com/go-openapi/swag v0.22.3 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/gnostic v0.5.7-v3refs // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/gofuzz v1.1.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/kr/text v0.2.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
 	github.com/moby/term v0.0.0-20220808134915-39b0c02b01ae // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/morikuni/aec v1.0.0 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.0.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
@@ -56,9 +73,22 @@ require (
 	github.com/prometheus/procfs v0.9.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/mod v0.10.0 // indirect
+	golang.org/x/oauth2 v0.7.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/term v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
 	golang.org/x/time v0.0.0-20220609170525-579cf78fd858 // indirect
 	golang.org/x/tools v0.9.1 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gotest.tools/v3 v3.4.0 // indirect
+	k8s.io/api v0.27.3 // indirect
+	k8s.io/klog/v2 v2.90.1 // indirect
+	k8s.io/kube-openapi v0.0.0-20230501164219-8b0f38b5fd1f // indirect
+	k8s.io/utils v0.0.0-20230209194617-a36077c30491 // indirect
+	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.2.3 // indirect
+	sigs.k8s.io/yaml v1.3.0 // indirect
 )
---
+++ internal/pkg/config/provider/k8s.go
@@ -0,0 +1,166 @@
+package provider
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"go.uber.org/zap"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+type KubernetesConfig struct {
+	AnnotationPrefix string        `mapstructure:"annotationPrefix"`
+	Namespace        string        `mapstructure:"namespace"`
+	ClientTimeout    time.Duration `mapstructure:"clientTimeout"`
+	Watch            bool          `mapstructure:"watch"`
+	Config           string        `mapstructure:"config"`
+}
+
+type Kubernetes struct {
+	KubernetesConfig
+	clientset *kubernetes.Clientset
+	logger    *zap.Logger
+}
+
+func NewKubernetes(cfg KubernetesConfig, logger *zap.Logger) Provider {
+	return &Kubernetes{
+		KubernetesConfig: cfg,
+		logger:           logger,
+	}
+}
+
+func (p *Kubernetes) Provide(dataCh chan<- Data) (Data, error) {
+	var config *rest.Config
+	var err error
+
+	// see if any config is specified at all and if so if we run inCluster
+	switch p.Config {
+	case "":
+		return Data{}, nil
+	case "inCluster":
+		config, err = clientcmd.BuildConfigFromFlags("", "")
+	default:
+		config, err = clientcmd.BuildConfigFromFlags("", p.Config)
+	}
+
+	if err != nil {
+		return Data{}, fmt.Errorf("could not load kube config file: %w", err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return Data{}, fmt.Errorf("could not create kube clientset: %w", err)
+	}
+
+	p.clientset = clientset
+
+	p.logger.Info("monitoring Kubernetes for Minecraft services")
+
+	cfg, err := p.readConfigData()
+	if err != nil {
+		return Data{}, err
+	}
+
+	if p.Watch {
+		go func() {
+			if err := p.watch(dataCh); err != nil {
+				p.logger.Error("failed while watching provider",
+					zap.Error(err),
+					zap.String("provider", KubernetesType.String()),
+				)
+			}
+		}()
+	}
+
+	return Data{
+		Type:   KubernetesType,
+		Config: cfg,
+	}, nil
+}
+
+func (p Kubernetes) readConfigData() (map[string]any, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), p.ClientTimeout)
+	defer cancel()
+
+	services, err := p.clientset.CoreV1().Services(p.Namespace).List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+
+	cfg := map[string]any{}
+	for _, service := range services.Items {
+		annotations := service.GetAnnotations()
+
+		for key, value := range annotations {
+			if !strings.HasPrefix(key, p.AnnotationPrefix) {
+				continue
+			}
+
+			key = strings.TrimPrefix(key, p.AnnotationPrefix)
+
+			if strings.HasPrefix(value, "[") {
+				value = strings.Trim(value, "[]")
+				setNestedValue(cfg, key, strings.Split(value, ","))
+			} else {
+				setNestedValue(cfg, key, value)
+			}
+		}
+
+	}
+	return cfg, nil
+}
+
+func (p Kubernetes) watch(dataCh chan<- Data) error {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	watcher, err := p.clientset.CoreV1().Services(p.Namespace).Watch(ctx, metav1.ListOptions{})
+	if err != nil {
+		return err
+	}
+
+	defer watcher.Stop()
+
+	ch := watcher.ResultChan()
+	for {
+		// Wait for any update
+		event := <-ch
+
+		// There will be an item in the channel for *every* service that
+		// is updated. Especially on start-up there will be a storm of
+		// events but we really want to update just once. So wait a short
+		// time for more events and then drain the channel.
+		time.Sleep(100 * time.Millisecond)
+		for drained := false; drained == false; {
+			select {
+			case <-ch:
+			default:
+				drained = true
+			}
+		}
+
+		if event.Type == watch.Added || event.Type == watch.Deleted {
+			cfg, err := p.readConfigData()
+			if err != nil {
+				p.logger.Info("failed to read data", zap.Error(err))
+				continue
+			}
+
+			dataCh <- Data{
+				Type:   KubernetesType,
+				Config: cfg,
+			}
+		}
+
+	}
+}
+
+func (p Kubernetes) Close() error {
+	return nil
+}
